da159153315cfcddfcb1548006bd7c1e8a21e9f3
Andy Pataki <anpataki@gmail.com>
2025-09-17 18:16:41 +0200

    First try with styled(); Relative base url needed for the build

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,45 @@
 import { useState } from "react";
 import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import "./overrides.css";
+import { ThemeProvider } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import customTheme from "./customTheme.ts";
+
+const StyledDiv = styled("div")({
+	backgroundColor: "white",
+	padding: 16,
+	marginTop: 16,
+	borderRadius: 8,
+	boxShadow: "0 0 5px 3px rgba(80, 116, 152, .7)",
+});
 
 function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<Container maxWidth="md">
-			<Box
-				sx={{
-					bgcolor: "background.default",
-					p: 4,
-					mt: 4,
-					borderRadius: 2,
-					boxShadow: 3,
-				}}
-			>
-				<Typography variant="h2" sx={{ mb: 2 }} color="text.primary">
-					Todo
-				</Typography>
-				<Button
-					color="primary"
-					variant="contained"
-					onClick={() => setCount((count) => count + 1)}
-				>
-					count is {count}
-				</Button>
-			</Box>
-		</Container>
+		<ThemeProvider theme={customTheme}>
+			<Container maxWidth="md">
+				<StyledDiv>
+					<Typography
+						variant="h2"
+						sx={{ mb: 2 }}
+						color="text.primary"
+					>
+						Todo
+					</Typography>
+					<Button
+						color="primary"
+						variant="contained"
+						onClick={() => setCount((count) => count + 1)}
+					>
+						count is {count}
+					</Button>
+				</StyledDiv>
+			</Container>
+		</ThemeProvider>
 	);
 }
 
---
+++ src/customTheme.ts
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+
+const customTheme = createTheme({
+	palette: {
+		primary: { main: "#507498" },
+	},
+});
+
+export default customTheme;
