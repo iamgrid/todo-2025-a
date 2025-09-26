713b84fbca73db0265a35a20863a55aed749dff4
Andy Pataki <anpataki@gmail.com>
2025-09-26 18:57:16 +0200

    Custom theme

--- src/customTheme.ts
+++ src/customTheme.ts
@@ -1,10 +1,62 @@
 import { createTheme } from "@mui/material";
 
+const colors = {
+	MAIN: "#507f98",
+	MAIN_DARK: "#0674a7",
+	MAIN_LIGHT: "#669cc3",
+	MAIN_EXTRA_LIGHT: "#50809850",
+	MAIN_EXTRA_LIGHT_B: "#50809810",
+};
+
 const customTheme = createTheme({
 	palette: {
-		primary: { main: "#507498" },
+		primary: {
+			main: colors.MAIN,
+			dark: colors.MAIN_DARK,
+			light: colors.MAIN_LIGHT,
+		},
 	},
-	shape: { borderRadius: 8 },
+	shape: { borderRadius: 12 },
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+					lineHeight: "normal",
+					"&.Mui-outlinedPrimary:hover": {
+						backgroundColor: colors.MAIN_EXTRA_LIGHT_B,
+						borderColor: colors.MAIN_EXTRA_LIGHT,
+					},
+					"&.Mui-containedPrimary:hover": {
+						backgroundColor: colors.MAIN_DARK,
+					},
+				},
+			},
+		},
+		MuiToggleButton: {
+			styleOverrides: {
+				root: {
+					textTransform: "none",
+					lineHeight: "normal",
+					paddingLeft: 12,
+					paddingRight: 12,
+					color: colors.MAIN,
+					borderColor: colors.MAIN_EXTRA_LIGHT,
+					"&.Mui-selected": {
+						backgroundColor: colors.MAIN,
+						color: "#fff",
+					},
+					"&.Mui-selected:hover": {
+						backgroundColor: colors.MAIN,
+					},
+					"&:hover": {
+						backgroundColor: colors.MAIN_EXTRA_LIGHT_B,
+					},
+					transition: "background-color 0.3s, color 0.3s",
+				},
+			},
+		},
+	},
 });
 
 export const buttonGroupWrapperStyle = {
